68e65f42ae5627c0c0a0031bf1a396f35862605e
boranes <boranes90@gmail.com>
2022-04-29 01:00:53 +0200

    Added redux auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,65 @@
+import AuthService from "../services/Auth/auth.service";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+} from "./types";
+
+export const register = (username, email, password) => (dispatch) => {
+  return AuthService.register(username, email, password).then(
+    ({ token, error }) => {
+      if (token) {
+        localStorage.setItem("currentUser", JSON.stringify(token));
+        dispatch({
+          type: REGISTER_SUCCESS,
+          payload: { currentUser: token },
+        });
+        return Promise.resolve();
+      }
+      if (error) {
+        dispatch({
+          type: REGISTER_FAIL,
+        });
+        return Promise.reject(error);
+      }
+    },
+    ({ error }) => {
+      dispatch({
+        type: REGISTER_FAIL,
+      });
+      return Promise.reject(error);
+    }
+  );
+};
+
+export const login = (email, password) => (dispatch) => {
+  return AuthService.login(email, password).then(
+    ({ token, error }) => {
+      if (token) {
+        localStorage.setItem("currentUser", JSON.stringify(token));
+        dispatch({
+          type: LOGIN_SUCCESS,
+          payload: { currentUser: token },
+        });
+        return Promise.resolve();
+      }
+      if (error) {
+        dispatch({ type: LOGIN_FAIL });
+        return Promise.reject(error);
+      }
+    },
+    ({ error }) => {
+      dispatch({ type: LOGIN_FAIL });
+      return Promise.reject(error);
+    }
+  );
+};
+
+export const logout = () => (dispatch) => {
+  AuthService.logout();
+  dispatch({
+    type: LOGOUT,
+  });
+};
