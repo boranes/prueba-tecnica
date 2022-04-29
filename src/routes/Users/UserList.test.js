5e72a08495dfb6db118dbab7c2215045967eb88f
boranes <boranes90@gmail.com>
2022-04-30 01:50:12 +0200

    Test to render a list of users

--- src/routes/Users/UserList.test.js
+++ src/routes/Users/UserList.test.js
@@ -15,4 +15,26 @@ describe("Testing User List Page", () => {
     render(<UserList />, { wrapper: Wrapper });
     expect(screen.getByText("No users were found")).toBeInTheDocument();
   });
+
+  test("Should render a list of users", () => {
+    const mockedUsers = [
+      {
+        id: 7,
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+      },
+      {
+        id: 8,
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+      },
+    ];
+    render(<UserList />, { wrapper: Wrapper });
+    const renderedUsers = screen.getAllByRole("listitem");
+    expect(renderedUsers.length).toEqual(mockedUsers.length);
+  });
 });
